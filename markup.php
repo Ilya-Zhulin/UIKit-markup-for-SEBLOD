1c02aa8152bd1ecb8f9bf3078b1b0cd87139e51e
Ilya A. Zhulin <ilya.zhulin@hotmail.com>
2021-08-16 13:08:06 +0300

    Create markup.php

---
+++ markup.php
@@ -0,0 +1,180 @@
+<?php
+
+/**
+ * @version 			SEBLOD 3.x Core
+ * @package			SEBLOD (App Builder & CCK) // SEBLOD nano (Form Builder)
+ * @url				http://www.seblod.com
+ * @editor			Octopoos - www.octopoos.com
+ * @copyright		Copyright (C) 2013 SEBLOD. All Rights Reserved.
+ * @license 			GNU General Public License version 2 or later; see _LICENSE.php
+ * */
+defined('_JEXEC') or die;
+
+// The markup around each field (label+value/form) can be Overridden.
+// Remove the underscore [_] from the Filename. (filename = markup.php)
+// Edit the function name:
+//	- fields/markup.php 			=>	cckMarkup_[template]
+//	- fields/[contenttype]/markup.php	=>	cckMarkup_[template]_[contenttype]
+//	- fields/[searchtype]/markup.php	=>	cckMarkup_[template]_[searchtype]
+// Write your Custom Markup code. (see default markup below)
+// cckMarkup
+
+function cckMarkup_seb_minima($cck, $html, $field, $options) {
+	$desc		 = '';
+	$addAttr	 = '';
+	$addClass	 = '';
+	$layout		 = '0';
+	if (stripos($cck->id_class, 'uk-form-stacked') !== FALSE) {
+		$layout = '1';
+	} elseif (stripos($cck->id_class, 'uk-form-horizontal') !== FALSE) {
+		$layout = '2';
+	}
+// Computation
+
+	if (isset($field->computation) && $field->computation) {
+		$cck->setComputationRules($field);
+	}
+
+// Conditional
+
+	if (isset($field->conditional) && $field->conditional) {
+		$cck->setConditionalStates($field);
+	}
+
+//	Description
+
+	if ($cck->getStyleParam('field_description', 0)) {
+		$desc = ( $field->description != '' ) ? '<div id="' . $cck->id . '_desc_' . $field->name . '" class="uk-text-small uk-form-controls cck_desc cck_desc_' . $field->type . '">' . $field->description . '</div>' : '';
+	}
+
+//	Label
+
+	$label = '';
+	if ($options->get('field_label', $cck->getStyleParam('field_label', 1))) {
+		if (strpos($field->markup_class, 'tyres-label-ph') !== FALSE && ($field->type == 'text' || $field->type == 'textarea')) {
+			$label	 = $cck->getLabel($field->name, false, ( $field->required ? '*' : ''));
+			$html	 = ( $label != '' ) ? str_replace(array('<input', '<textarea'), array('<input placeholder="' . $label . '"', '<textarea placeholder="' . $label . '"'), $html) : $html;
+			$label	 = '';
+		} else {
+			$label	 = $cck->getLabel($field->name, true, ( $field->required ? '*' : ''));
+			$label	 = ( $label != '' ) ? str_replace('<label', '<label class="uk-form-label"', $label) : '';
+		}
+	}
+
+
+//	Field Types
+
+	switch ($field->type) {
+		case 'checkbox':
+		case 'checkbox_dynamic':
+			$html	 = preg_replace('/class=\"([^\"]*)(uk-input)([^\"]*)\"/', 'class="$1uk-checkbox $3"', $html);
+			$html	 = preg_replace('/label><input([^>]*optgroup[^>]*)><label([^>]*)>([^>]*label>)/', 'label><span class="uk-clearfix"></span><label class="uk-form-label uk-text-bold uk-clearfix">$3', $html);
+			$html	 = preg_replace('/"><input([^>]*optgroup[^>]*)><label([^>]*)>([^>]*label>)/', '"><label class="uk-form-label uk-text-bold uk-clearfix">$3', $html);
+			break;
+
+		case 'textarea':
+			$html = preg_replace('/class=\"([^\-"]*)(uk-input)([^\"]*)\"/', 'class="$1uk-textarea $3"', $html);
+			break;
+
+		case 'group_x':
+			$html	 = preg_replace('/class=\\"([^\\"]*)(auto-expand)([^\\"]*)\\"/', 'uk-grid', $html);
+			$html	 = preg_replace('/class=\\"([^\\"]*)(cck_cgx_button)([^\\"]*)\\"/', 'class="$1 uk-float-right $3"', $html);
+			$html	 = preg_replace('/class=\\"([^\\"]*)(cck_cgx_form)([^\\"]*)\\"/', 'class="$1 uk-width-1-1 $3"', $html);
+			$html	 = preg_replace('/class=\\"([^\\"]*)(uk-form-controls)([^\\"]*)\\"/', 'class="$1 uk-margin-remove-left $3"', $html);
+			$html	 = preg_replace('/class=\\"([^\\"]*)(cck_form_group_x)([^\\"]*)\\"/', 'class="$1 uk-clearfix uk-margin $3"', $html);
+			$html	 = preg_replace('/class=\\"([^\\"]*)(cck_wysiwyg_editor)([^\\"]*)\\"/', 'class="$1 uk-clearfix uk-margin $3"', $html);
+			$html	 = str_replace('<input type="file"', '<div uk-form-custom="target: true" class="uk-form-custom uk-width-1-1"><input class="target uk-input uk-width-1-1" type="text" placeholder="Кликните, чтобы выбрать файл" disabled=""><input type="file', $html);
+			$html	 = str_replace('<span class="hasTooltip" title="Check to delete the file">', '</div><span class="hasTooltip" title="Check to delete the file">', $html);
+			break;
+
+		case 'radio':
+			$html	 = str_replace('class="radio"', 'class="uk-radio"', $html);
+			$html	 = str_replace('class="radios"', 'class="uk-margin uk-grid-small uk-child-width-auto uk-grid"', $html);
+			$html	 = str_replace('fieldset', 'div', $html);
+			if (stripos($field->attributes, 'data-input-label') === FALSE) {
+				$html	 = preg_replace("/(<label for=\"([^\"]*)\">)/u", "", $html);
+				$html	 = preg_replace("/(<input[^>]*id=\"([^\"]*)\"[^>]*>)/u", "<label for='$2'>$1 ", $html);
+				$html	 = str_replace('</label>', '</label>', $html);
+			}
+			if ($label != '') {
+				$html = '<div ' . $addAttr . 'class="uk-form-controls ' . $field->name . ' ' . $field->type . $addClass . '">' . $html . '</div>';
+			} else {
+				$html = '<div class="' . $field->name . ' ' . $field->type . $addClass . '">' . $html . '</div>';
+			}
+			break;
+
+		case 'select_simple':
+			$html = preg_replace('/class=\"([^\-"]*)(select)([^\"]*)\"/', 'class="$1uk-select $3"', $html);
+			break;
+
+		case 'text':
+			$html = preg_replace('/class=\"([^\"]*)(text)([^\"]*)\"/', 'class="$1uk-input $3"', $html);
+			break;
+
+		case 'upload_image':
+			$html	 = preg_replace('/class=\"([^\"]*)(input)([^\"]*)\"([^>])(type="checkbox")(.*)>/', 'class="$1uk-checkbox $3"$4$5$6>', $html);
+			$html	 = preg_replace('/class=\"([^\"]*)(input)([^\"]*)\"/', 'class="$1uk-input $3"', $html);
+			break;
+	}
+// Fields layouts
+	if ($layout > 0) {
+		$html = '<div class="uk-form-controls">' . $html . '</div>';
+	}
+
+
+	if ($field->type == 'checkbox' || $field->type == 'upload_image') {
+		$html	 = str_replace(array('class="checkbox ', 'class="checkbox"'), array('class="uk-checkbox ', 'class="uk-checkbox"'), $html);
+		$html	 = str_replace('class="checkboxes', 'class="uk-fieldset', $html);
+		$html	 = str_replace('fieldset', 'div', $html);
+		if (stripos($field->attributes, 'data-input-label') === FALSE) {
+			$html	 = preg_replace("/(<label for=\"([^\"]*)\">)/u", "", $html);
+			$html	 = preg_replace("/(<input[^>]*id=\"([^\"]*)\"[^>]*>)/u", "<label class='uk-margin-right' for='$2'>$1", $html);
+		}
+	}
+
+	if (stripos($field->attributes, 'data-form-icon') !== FALSE && ($cck->client == 'site' || $cck->client == 'search')) {
+//		jbdump($cck->client);
+		$attr	 = $field->attributes;
+		$attr	 = str_replace(' ', '~', $attr);
+		$attr	 = str_replace('"~', '" ', $attr);
+		$attr	 = str_replace('~', ' ', $attr);
+		$attrs	 = explode(' ', trim($attr));
+		$icon	 = '';
+		$flip	 = '';
+		$tag	 = "span";
+		$tooltip = '';
+		foreach ($attrs as $attrib) {
+			if (stripos($attrib, 'data-form-icon') !== FALSE) {
+				$vals = explode('=', trim($attrib));
+				if ($vals[0] == 'data-form-icon') {
+					$icon = (strlen($icon) > 0) ? $icon : str_replace('"', '', $vals[1]);
+				}
+				if ($vals[0] == 'data-form-icon-tooltip') {
+					$tooltip = ' uk-tooltip title="' . str_replace('"', '', $vals[1]) . '"';
+				}
+				if ($vals[0] == 'data-form-icon-flip') {
+					$flip	 = " uk-form-icon-flip";
+					$icon	 = (strlen($icon) > 0) ? $icon : str_replace('"', '', $vals[1]);
+				}
+				if ($vals[0] == 'data-form-icon-flip-a') {
+					$flip	 = " uk-form-icon-flip";
+					$tag	 = "a";
+					$icon	 = (strlen($icon) > 0) ? $icon : str_replace('"', '', $vals[1]);
+				} elseif ($vals[0] == 'data-form-icon-a') {
+					$tag	 = "a href='#'";
+					$icon	 = (strlen($icon) > 0) ? $icon : str_replace('"', '', $vals[1]);
+				}
+			}
+		}
+		$html = '<div id="' . $cck->id . '_' . $cck->mode_property . '_' . $field->name . '" class="uk-inline uk-width-1-1"><' . $tag .
+				' class="uk-form-icon' . $flip . '" uk-icon="icon: ' . $icon . '"' . $tooltip . '></' . $tag .
+				'>' . $html . '</div>';
+	}
+
+
+	$html = '<div id="' . $cck->id . '_' . $field->name . '"  class="uk-margin ' . $field->name . ' ' . $field->markup_class . '">' . $label . $html . $desc . '</div>';
+
+	return $html;
+}
+
+?>
