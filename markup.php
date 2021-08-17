b3224e72ecf31dbd6fbe96da31706e010386e341
Ilya A. Zhulin <ilya.zhulin@hotmail.com>
2021-08-17 23:26:29 +0300

    Исправления для checkbox
    Разделил формирование разметки для checkbox и checkbox_dynamic

--- markup.php
+++ markup.php
@@ -29,6 +29,7 @@ function cckMarkup_seb_minima($cck, $html, $field, $options) {
 	} elseif (stripos($cck->id_class, 'uk-form-horizontal') !== FALSE) {
 		$layout = '2';
 	}
+
 // Computation
 
 	if (isset($field->computation) && $field->computation) {
@@ -66,8 +67,16 @@ function cckMarkup_seb_minima($cck, $html, $field, $options) {
 
 	switch ($field->type) {
 		case 'checkbox':
+			$html	 = str_replace(array('class="checkbox ', 'class="checkbox"'), array('class="uk-checkbox uk-margin-small-right ', 'class="uk-checkbox uk-margin-small-right"'), $html);
+			$html	 = str_replace('class="checkboxes', 'class="uk-fieldset', $html);
+			$html	 = str_replace('fieldset', 'div', $html);
+			if (stripos($field->attributes, 'data-input-label') === FALSE) {
+				$html	 = preg_replace("/(<label for=\"([^\"]*)\">)/u", "", $html);
+				$html	 = preg_replace("/(<input[^>]*id=\"([^\"]*)\"[^>]*>)/u", "<label class='uk-margin-right' for='$2'>$1", $html);
+			}
+			break;
 		case 'checkbox_dynamic':
-			$html	 = preg_replace('/class=\"([^\"]*)(uk-input)([^\"]*)\"/', 'class="$1uk-checkbox $3"', $html);
+			$html	 = preg_replace('/class=\"([^\"]*)(uk-input)([^\"]*)\"/', 'class="$1uk-checkbox uk-margin-right-small $3"', $html);
 			$html	 = preg_replace('/label><input([^>]*optgroup[^>]*)><label([^>]*)>([^>]*label>)/', 'label><span class="uk-clearfix"></span><label class="uk-form-label uk-text-bold uk-clearfix">$3', $html);
 			$html	 = preg_replace('/"><input([^>]*optgroup[^>]*)><label([^>]*)>([^>]*label>)/', '"><label class="uk-form-label uk-text-bold uk-clearfix">$3', $html);
 			break;
@@ -78,13 +87,29 @@ function cckMarkup_seb_minima($cck, $html, $field, $options) {
 
 		case 'group_x':
 			$html	 = preg_replace('/class=\\"([^\\"]*)(auto-expand)([^\\"]*)\\"/', 'uk-grid', $html);
-			$html	 = preg_replace('/class=\\"([^\\"]*)(cck_cgx_button)([^\\"]*)\\"/', 'class="$1 uk-float-right $3"', $html);
+			$html	 = preg_replace('/class=\\"([^\\"]*)(cck_cgx_button)([^\\"]*)\\"/', 'class="$1 uk-float-right uk-iconnav uk-margin-bottom $3"', $html);
 			$html	 = preg_replace('/class=\\"([^\\"]*)(cck_cgx_form)([^\\"]*)\\"/', 'class="$1 uk-width-1-1 $3"', $html);
 			$html	 = preg_replace('/class=\\"([^\\"]*)(uk-form-controls)([^\\"]*)\\"/', 'class="$1 uk-margin-remove-left $3"', $html);
 			$html	 = preg_replace('/class=\\"([^\\"]*)(cck_form_group_x)([^\\"]*)\\"/', 'class="$1 uk-clearfix uk-margin $3"', $html);
-			$html	 = preg_replace('/class=\\"([^\\"]*)(cck_wysiwyg_editor)([^\\"]*)\\"/', 'class="$1 uk-clearfix uk-margin $3"', $html);
+			$html	 = preg_replace('/class=\\"([^\\"]*)(cck_wysiwyg_editor)([^\\"]*)\\"/', 'class="$1 uk-clearfix uk-margin1 $3"', $html);
 			$html	 = str_replace('<input type="file"', '<div uk-form-custom="target: true" class="uk-form-custom uk-width-1-1"><input class="target uk-input uk-width-1-1" type="text" placeholder="Кликните, чтобы выбрать файл" disabled=""><input type="file', $html);
 			$html	 = str_replace('<span class="hasTooltip" title="Check to delete the file">', '</div><span class="hasTooltip" title="Check to delete the file">', $html);
+			// Inputs
+			$html	 = preg_replace('/class=\"([^\"_]*)(text)([^\"]*)\"/', 'class="$1 uk-input $3"', $html);
+			// Selects
+			$html	 = preg_replace('/class=\"([^\-"]*)(select)([^\"]*)\"/', 'class="$1uk-select $3"', $html);
+			// Labels
+			$html	 = str_replace('<label', '<label class="uk-form-label"', $html);
+			$html	 = preg_replace('/<div([^>]*)>(<label([^>]*)>([^<]*)<\/label>)<\/div>/', '$2', $html);
+			$html	 = preg_replace('/<\/label><div([^>]*)class="([^\"]*)"/', '</label><div$1class="uk-form-controls $2"', $html);
+			// GroupX
+			$html	 = str_replace('<span class="icon-minus"></span>', '<li><a href="#" uk-icon="icon: minus-circle" class="uk-text-danger icon-minus"></a></li>', $html);
+			$html	 = str_replace('<span class="icon-plus"></span>', '<li><a href="#" onclick="return false" uk-icon="icon: plus-circle" class="uk-text-success icon-plus"></a></li>', $html);
+			$html	 = str_replace('<span class="icon-circle"></span>', '<li><a href="#" uk-icon="icon: move" class="uk-text-primary icon-circle"></a></li>', $html);
+			$html	 = str_replace('aside', 'ul', $html);
+			$html	 = preg_replace('/<div([^>]*)>(<label([^>]*)>([^<]*)<\/label>)<\/div>/', '$2', $html);
+			$html	 = preg_replace('/<div([^>]*)>(<li>)(.*?)(<\/li>)<\/div>/si', '<li$1>$3</li>', $html);
+
 			break;
 
 		case 'radio':
@@ -114,22 +139,18 @@ function cckMarkup_seb_minima($cck, $html, $field, $options) {
 		case 'upload_image':
 			$html	 = preg_replace('/class=\"([^\"]*)(input)([^\"]*)\"([^>])(type="checkbox")(.*)>/', 'class="$1uk-checkbox $3"$4$5$6>', $html);
 			$html	 = preg_replace('/class=\"([^\"]*)(input)([^\"]*)\"/', 'class="$1uk-input $3"', $html);
+			$html	 = str_replace(array('class="checkbox ', 'class="checkbox"'), array('class="uk-checkbox uk-margin-small-right ', 'class="uk-checkbox uk-margin-small-right"'), $html);
+			$html	 = str_replace('class="checkboxes', 'class="uk-fieldset', $html);
+			$html	 = str_replace('fieldset', 'div', $html);
+			if (stripos($field->attributes, 'data-input-label') === FALSE) {
+				$html	 = preg_replace("/(<label for=\"([^\"]*)\">)/u", "", $html);
+				$html	 = preg_replace("/(<input[^>]*id=\"([^\"]*)\"[^>]*>)/u", "<label class='uk-margin-right' for='$2'>$1", $html);
+			}
 			break;
 	}
 // Fields layouts
-	if ($layout > 0) {
+	if ($layout > 0 && strlen($label) > 0) {
 		$html = '<div class="uk-form-controls">' . $html . '</div>';
-	}
-
-
-	if ($field->type == 'checkbox' || $field->type == 'upload_image') {
-		$html	 = str_replace(array('class="checkbox ', 'class="checkbox"'), array('class="uk-checkbox ', 'class="uk-checkbox"'), $html);
-		$html	 = str_replace('class="checkboxes', 'class="uk-fieldset', $html);
-		$html	 = str_replace('fieldset', 'div', $html);
-		if (stripos($field->attributes, 'data-input-label') === FALSE) {
-			$html	 = preg_replace("/(<label for=\"([^\"]*)\">)/u", "", $html);
-			$html	 = preg_replace("/(<input[^>]*id=\"([^\"]*)\"[^>]*>)/u", "<label class='uk-margin-right' for='$2'>$1", $html);
-		}
 	}
 
 	if (stripos($field->attributes, 'data-form-icon') !== FALSE && ($cck->client == 'site' || $cck->client == 'search')) {
